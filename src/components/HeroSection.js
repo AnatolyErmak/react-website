ee522b4a109d2e09e03d35d038fad574c99bd9dc
Anatoly Ermakov <Pr0f3ssAr@yandex.ru>
2020-09-23 00:21:22 +0300

    create Hero Section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import {
+	Button
+} from './Button'
+import {
+	Link
+} from 'react-router-dom'
+import './HeroSection.css'
+
+
+function HeroSection({
+	LightBg,
+	topLine,
+	LightText,
+	LightTextDesc,
+	headline,
+	description,
+	buttonLabel,
+	img,
+	alt,
+	imgStart
+}) {
+	return ( <
+		>
+		<
+		div className = {
+			LightBg ? 'home__hero-section' : 'home__hero-section darkBg'
+		} >
+		<
+		div className = "container" >
+		<
+		div className = "row home__hero-row"
+		style = {
+			{
+				display: 'flex',
+				flexDirection: imgStart === 'start' ?
+					'row-reverse' : 'row'
+			}
+		} >
+		<
+		div className = "col" >
+		<
+		div className = "home__hero-text-wrapper" >
+		<
+		div className = "top-line" > {
+			topLine
+		} < /div> <
+		/div> <
+		/div> <
+		/div> <
+		/div> <
+		/div> <
+		/>
+	)
+}
+
+export default HeroSection
