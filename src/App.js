3e075e4e328667c2fd24aa1db405bcfe92563007
Anatoly Ermakov <Pr0f3ssAr@yandex.ru>
2020-09-23 18:06:31 +0300

    Creating the Homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Navbar from './components/Navbar'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+import Home from './components/pages/HomePage/Home'
+
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path ='/'/>
-      </Switch>
+  return ( 
+  <Router>
+    <Navbar />
+    <Switch >
+    <Route path = '/' exact component = { Home}/> 
+    </Switch> 
     </Router>
   );
 }
